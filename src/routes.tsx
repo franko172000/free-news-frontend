06c0d214f5ee64b12720448baa1ff28ab3cf6a8d
Franklin Anyaso <franko172000@gmail.com>
2023-06-07 20:46:27 +0100

    Setup tailwind and routes

---
+++ src/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Homepage from "./pages/Homepage";
+
+export default createBrowserRouter ([
+    {
+        path: '/',
+        element: <Homepage />
+    }
+])
